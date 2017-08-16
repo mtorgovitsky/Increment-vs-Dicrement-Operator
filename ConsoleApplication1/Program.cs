76e78983a32e0908c7f7b90c5c393b8a160262b2
s036190171 <s036190171@LY090825>
2017-08-16 12:21:14 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 6;
+            int var2 = 6;
+            Console.WriteLine($"Var1 before: (var1) {var1}");
+            Console.WriteLine($"Var1 after: (++var1) {++var1}");
+            Console.WriteLine($"Var1 after and after: (var1) {var1}");
+            Console.WriteLine($"Var2 before: (var2) {var2}");
+            Console.WriteLine($"Var2 after: (var2++) {var2++}");
+            Console.WriteLine($"Var2 after and after: (var2++) {var2}");
+            Console.ReadKey();
+        }
+    }
+}
